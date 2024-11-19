5df05bd8cb756dbcc7a4a039757541e6bdbd30c2
Anthony Guimaraes <146374745+Anthony-Sutil@users.noreply.github.com>
2024-11-19 02:28:06 -0300

    helpe me

--- models/sensor.py
+++
@@ -1,6 +0,0 @@
-class Sensor(db.Model):
-    __tablename__ = 'sensores'
-    id = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(50), nullable=False)  # abelhas, vacas
-    leitura_atual = db.Column(db.String(200), nullable=True)
-    device_id = db.Column(db.Integer, db.ForeignKey('devices.id'), nullable=False)
